9e7a1e5a575a0976190f79b9b05f0396e737dd48
Allan Kiche <tle2819@students.mu.ac.ke>
2023-02-25 13:15:46 +0300

    roman2integers

---
+++ RomanNumeralsToIntergers.py
@@ -0,0 +1,27 @@
+def roman_to_int(roman):
+    values = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+        'IV': 4,
+        'IX': 9,
+        'XL': 40,
+        'XC': 90,
+    }
+    
+    result = 0
+    prev_value = 0
+    
+    for char in roman[::-1]:
+        value = values[char]
+        if value < prev_value:
+            result -= value
+        else:
+            result += value
+            
+        prev_value = value
+    
