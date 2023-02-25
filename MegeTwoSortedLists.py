2a410113c75da58f6bb000ec1e87bbf8294a1c94
Allan Kiche <tle2819@students.mu.ac.ke>
2023-02-25 13:06:40 +0300

    roman

--- MegeTwoSortedLists.py
+++ MegeTwoSortedLists.py
@@ -14,10 +14,10 @@ print(original_list2)
 
 #we can merge two sorted lists i  python by using a simple algorithm that compares these two lists and adds the smaller element to the result list. Then it moves the pointer of the list with the smaller element to the next elementand continues tee process until one of the lists is completely empty. Finally it appends the remaining elements of the non-empty list to the new list
 
-def merge_sorted_lists(originallist1, originallist2):
+def merge_sorted_lists(original_list1, original_list2):
     merged_list = []
     i, j =0, 0
-    while i < len(originallist1) and j < len(originallist2):
+    while i < len(original_list1) and j < len(original_list2):
         if original_list1[i] < original_list2[j]:
             merged_list.append(original_list1[i])
             i += 1
@@ -29,3 +29,5 @@ def merge_sorted_lists(originallist1, originallist2):
     merged_list += original_list1[i:] + original_list2[j:]
     return merged_list
 print(merge_sorted_lists(original_list1, original_list2))
+
+# After the while loop has completed, we have finished comparing all the elements in the input lists. However, we may have some remaining elements in one of the lists that we haven't yet added to the merged_list. To add these remaining elements, we concatenate the slices originallist1[i:] and originallist2[j:] to the merged_list. These slices contain all the remaining elements in originallist1 and originallist2, respectively.
